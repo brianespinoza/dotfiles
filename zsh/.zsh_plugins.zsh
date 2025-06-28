69370ef527fee74a2b73efa725ef0d72c36b5ea3
Brian Espinoza <brian.espinoza@rhombus.com>
2025-06-27 21:43:36 -0700

    add zshplugins

---
+++ zsh/.zsh_plugins.zsh
@@ -0,0 +1,28 @@
+fpath+=( /opt/homebrew/opt/antidote/share/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-romkatv-SLASH-powerlevel10k )
+source /opt/homebrew/Cellar/antidote/1.9.7/share/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-romkatv-SLASH-powerlevel10k/powerlevel10k.zsh-theme
+source /opt/homebrew/Cellar/antidote/1.9.7/share/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-romkatv-SLASH-powerlevel10k/powerlevel9k.zsh-theme
+fpath+=( /opt/homebrew/opt/antidote/share/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-getantidote-SLASH-use-omz )
+source /opt/homebrew/Cellar/antidote/1.9.7/share/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-getantidote-SLASH-use-omz/antidote-use-omz.plugin.zsh
+source /opt/homebrew/Cellar/antidote/1.9.7/share/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-getantidote-SLASH-use-omz/use-omz.plugin.zsh
+fpath+=( /opt/homebrew/opt/antidote/share/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/git )
+source /opt/homebrew/Cellar/antidote/1.9.7/share/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/git/git.plugin.zsh
+fpath+=( /opt/homebrew/opt/antidote/share/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/gitignore )
+source /opt/homebrew/Cellar/antidote/1.9.7/share/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/gitignore/gitignore.plugin.zsh
+fpath+=( /opt/homebrew/opt/antidote/share/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/aliases )
+source /opt/homebrew/Cellar/antidote/1.9.7/share/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/aliases/aliases.plugin.zsh
+fpath+=( /opt/homebrew/opt/antidote/share/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/common-aliases )
+source /opt/homebrew/Cellar/antidote/1.9.7/share/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/common-aliases/common-aliases.plugin.zsh
+if ! (( $+functions[zsh-defer] )); then
+  fpath+=( /opt/homebrew/opt/antidote/share/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-romkatv-SLASH-zsh-defer )
+  source /opt/homebrew/Cellar/antidote/1.9.7/share/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-romkatv-SLASH-zsh-defer/zsh-defer.plugin.zsh
+fi
+fpath+=( /opt/homebrew/opt/antidote/share/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/ngrok )
+zsh-defer source /opt/homebrew/Cellar/antidote/1.9.7/share/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/ngrok/ngrok.plugin.zsh
+fpath+=( /opt/homebrew/opt/antidote/share/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-Aloxaf-SLASH-fzf-tab )
+zsh-defer source /opt/homebrew/Cellar/antidote/1.9.7/share/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-Aloxaf-SLASH-fzf-tab/fzf-tab.plugin.zsh
+fpath+=( /opt/homebrew/opt/antidote/share/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zdharma-continuum-SLASH-fast-syntax-highlighting )
+zsh-defer source /opt/homebrew/Cellar/antidote/1.9.7/share/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zdharma-continuum-SLASH-fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh
+fpath+=( /opt/homebrew/opt/antidote/share/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions )
+zsh-defer source /opt/homebrew/Cellar/antidote/1.9.7/share/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
+fpath+=( /opt/homebrew/opt/antidote/share/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-history-substring-search )
+zsh-defer source /opt/homebrew/Cellar/antidote/1.9.7/share/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-history-substring-search/zsh-history-substring-search.plugin.zsh
